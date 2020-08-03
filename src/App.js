d485a84b65080d3237f9748e8d30c16736c3f54b
Ryan Hall <ryanhall999@gmail.com>
2020-08-02 20:00:00 -0600

    button

--- src/App.js
+++ src/App.js
@@ -3,7 +3,24 @@ import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import axios from "axios";
 
 function App() {
-	return <div>Hello World</div>;
+	const handleClick = async (e) => {
+		e.preventDefault();
+		console.log("works");
+		// await axios
+		// 	.get(
+		// 		"https://www.fantasyfootballnerd.com/service/{SERVICE-NAME}/{FORMAT}/{API-KEY}"
+		// 	)
+		// 	.then((response) => {})
+		// 	.catch((error) => {
+		// 		console.log(error);
+		// 	});
+	};
+
+	return (
+		<div>
+			<button onClick={handleClick}>Click Me</button>
+		</div>
+	);
 }
 
 export default App;
